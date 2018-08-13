b24b593d3c4d64bab465ee60b4ee1e0c49116ecc
zon <zqzhong1211@163.com>
2018-08-13 11:45:56 +0800

    refactor: des modify.

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ Zento is a configuration-based solution to form.
 ## TODO
 
 * [x] 表单配置化生成
-* [] 支持表单联动配置
+* [ ] 支持表单联动配置
--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -5,15 +5,12 @@ const baseWebpackConfig = require('./webpack.base.config');
 
 module.exports = merge(baseWebpackConfig, {
   entry: {
-    bundle: './src/index.js'
+    bundle: './docs/src/index.js'
   },
   output: {
-    path: path.resolve(__dirname, '../lib'),
-    filename: 'App.js',
-    libraryTarget: 'commonjs2'
-  },
-  externals: {
-    react: 'react'
+    path: path.resolve(__dirname, '../docs/dist'),
+    publicPath: 'docs/dist/',
+    filename: '[name].js'
   },
   plugins: [
     new webpack.DefinePlugin({
