70d75d809317318c9fb4df5cdf75f371d91713be
zon <zqzhong1211@163.com>
2018-08-17 23:57:02 +0800

    feat: demo.

---
+++ docs/src/code.js
@@ -0,0 +1,45 @@
+const reactCode = `
+import { FormCreator, fieldComponentCreator } from 'zento';
+import infoForm from './config/infoForm';
+import componentMap from './config/componentMap';
+import formFieldOptions from './config/formFieldOptions';
+
+const fieldCreator = fieldComponentCreator(componentMap, formFieldOptions);
+const formFieldRender = (bindField, config) => {
+  const { label, name, fieldType, extra, rule } = config;
+  return (
+    <FormItem colon label={label}>
+       {bindField(name, {
+         rules: [rule],
+       })(fieldCreator(fieldType, name, extra))}
+     </FormItem>
+  );
+};
+const formCreator = new FormCreator({ formFieldRender });
+
+<Form layout="inline">
+  {formCreator.create(infoForm, getFieldDecorator, [activeIndex])}
+</Form>
+`;
+
+const componentMapCode = `
+import { Input, DatePicker } from 'antd';
+import AddressCascader from '../components/AddressCascader';
+import InfoRadio from '../components/InfoRadio';
+import InfoSelect from '../components/InfoSelect';
+
+const componentMap = {
+  input: Input,
+  datePicker: DatePicker,
+  radio: InfoRadio,
+  select: InfoSelect,
+  address: AddressCascader
+};
+
+export default componentMap;
+`;
+
+export {
+  reactCode,
+  componentMapCode
+};
--- docs/src/index.js
+++ docs/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Button, Form } from 'antd';
+import SyntaxHighlighter from 'react-syntax-highlighter/prism';
+import { docco } from 'react-syntax-highlighter/styles/prism';
 import FormCreator from '../../src/formCreator';
 import fieldComponentCreator from '../../src/fieldComponentCreator';
 import Tabs from './components/Tabs';
 import infoForm from './config/infoForm';
-import fieldInitValueMap from './config/fieldInitValueMap';
 import componentMap from './config/componentMap';
 import formFieldOptions from './config/formFieldOptions';
+import { reactCode, componentMapCode } from './code';
 import './index.scss';
 
 const FormItem = Form.Item;
@@ -23,13 +25,9 @@ const formFieldRender = (bindField, config) => {
   );
 };
 const formCreator = new FormCreator({ formFieldRender });
-const getStyleWrapper = () => ({
-  padding: 30,
-  boxSizing: 'border-box',
-  width: 1100,
-  margin: '0 auto',
-  border: '1px solid #e5e5e5'
-});
+
+const infoFormCode = JSON.stringify(infoForm, null, 2);
+const formFieldOptionsCode = JSON.stringify(formFieldOptions, null, 2);
 
 class Hello extends React.Component {
   constructor(props) {
@@ -53,14 +51,33 @@ class Hello extends React.Component {
     const { activeIndex } = this.state;
     const { getFieldDecorator } = this.props.form;
     return (
-      <div style={getStyleWrapper()}>
-        <h1 style={{ textAlign: 'center', paddingBottom: 20 }}>Zento 🍱</h1>
-        <Tabs data={infoForm} activeIndex={activeIndex} handlePanelClick={this.handlePanelClick} />
-        <Form layout="inline">
-          {formCreator.create(infoForm, getFieldDecorator, [activeIndex])}
-        </Form>
-        <div style={{ textAlign: 'center', marginTop: 20 }}>
-          <Button type="primary" onClick={this.handleSubmit}>提交</Button>
+      <div style={{ width: 1100, margin: '0 auto' }}>
+        <div
+          style={{
+            padding: 30,
+            boxSizing: 'border-box',
+            border: '1px solid #e5e5e5'
+          }}
+        >
+          <h1 style={{ textAlign: 'center', paddingBottom: 20 }}>Zento 🍱</h1>
+          <Tabs data={infoForm} activeIndex={activeIndex} handlePanelClick={this.handlePanelClick} />
+          <Form layout="inline">
+            {formCreator.create(infoForm, getFieldDecorator, [activeIndex])}
+          </Form>
+          <div style={{ textAlign: 'center', marginTop: 20 }}>
+            <Button type="primary" onClick={this.handleSubmit}>提交</Button>
+          </div>
+        </div>
+        <div style={{ paddingTop: 20 }}>
+          <h2>Code Snippet</h2>
+          <h3>Demo</h3>
+          <SyntaxHighlighter language='javascript' style={docco}>{reactCode}</SyntaxHighlighter>
+          <h3>infoForm</h3>
+          <SyntaxHighlighter language='javascript' style={docco}>{infoFormCode}</SyntaxHighlighter>
+          <h3>componentMap</h3>
+          <SyntaxHighlighter language='javascript' style={docco}>{componentMapCode}</SyntaxHighlighter>
+          <h3>formFieldOptions</h3>
+          <SyntaxHighlighter language='javascript' style={docco}>{formFieldOptionsCode}</SyntaxHighlighter>
         </div>
       </div>
     );
