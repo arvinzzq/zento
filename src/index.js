6a597b56620b33b96db8c93e2f21587684245fce
zon <zqzhong1211@163.com>
2018-08-18 12:31:58 +0800

    fix: fix export of methods.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import fieldComponentCreator from './fieldComponentCreator';
 import fieldNameRuleCreator from './fieldNameRuleCreator';
 import fieldSymbols from './fieldSymbols';
-import formCreator from './formCreator';
+import FormCreator from './formCreator';
 import formElementMap from './formElementMap';
 
 export {
